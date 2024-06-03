9c0203ba0d33d59965b12a965a61c151d8dc450b
Trong Tu Le <letrongtu203@gmail.com>
2024-06-03 22:58:38 +0800

    Final

--- main.cpp
+++ main.cpp
@@ -9,8 +9,6 @@
 /**
  * @author Trong Tu Le
  * @version 1
- * Read the data from a csv file, store all required data into a WindRecType type Vector. Calculate average speed, standard deviation,
- * and find the data that match with the average speed
  * @version 2
  * Separate additional function declarations and implementations
  */
